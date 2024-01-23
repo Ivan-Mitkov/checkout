921ba09dd8a138e146a67ad0c0511f851de88fbf
Ivan Mitkov <ivan.mitkov@gmail.com>
2024-01-23 12:35:27 +0200

    add styles

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar } from "../../components";
 
+import styles from "./ErrorPage.module.scss";
 const ErrorPage = () => {
   return (
     <div>
-      <h1>Whoops!</h1>
-      <h2>This page got lost in conversation.</h2>
-      <p>Not to worry. You can go to our homepage</p>
+      <AppBar />
+      <div className={styles.container}>
+        <h1>Whoops!</h1>
+        <h2>This page got lost in conversation.</h2>
+        <p>
+          Not to worry. You can go to our{" "}
+          <Link to="/" className={styles.link}>
+            homepage
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
